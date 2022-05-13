f1cc6ba434aa6393584cc199b94a2d9fd9468a1c
João Pedro Moraes <joao.moraes@capela.com.br>
2022-05-13 17:15:38 -0300

    Mapeando entidades com JPA

---
+++ src/main/java/br/com/moraesit/moraesfood/domain/entity/Cozinha.java
@@ -0,0 +1,49 @@
+package br.com.moraesit.moraesfood.domain.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Cozinha {
+
+    @Id
+    private Long id;
+
+    private String nome;
+
+    public Cozinha(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cozinha cozinha = (Cozinha) o;
+
+        return id.equals(cozinha.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}

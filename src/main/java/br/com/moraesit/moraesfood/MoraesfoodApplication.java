1bfeb9e23256459a142cbc567b352d47c5e9593b
João Pedro Moraes <joao.moraes@capela.com.br>
2022-05-13 13:33:43 -0300

    Criando um projeto Spring Boot com Spring Initializr

---
+++ src/main/java/br/com/moraesit/moraesfood/MoraesfoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.moraesit.moraesfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoraesfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoraesfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/moraesit/moraesfood/MoraesfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moraesit.moraesfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoraesfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

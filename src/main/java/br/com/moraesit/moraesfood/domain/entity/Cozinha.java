5bc7237305e334cac08915f03e8e8d9d87a16889
João Pedro Moraes <joao.moraes@capela.com.br>
2022-05-14 14:03:51 -0300

    Customizando as representações XML e JSON com @JsonIgnore, @JsonProperty e @JsonRootName

--- src/main/java/br/com/moraesit/moraesfood/domain/entity/Cozinha.java
+++ src/main/java/br/com/moraesit/moraesfood/domain/entity/Cozinha.java
@@ -1,11 +1,15 @@
 package br.com.moraesit.moraesfood.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@JsonRootName("cozinha")
 @Entity
 @Data
 @EqualsAndHashCode(of = {"id"})
@@ -16,6 +20,8 @@ public class Cozinha {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    //@JsonIgnore
+    @JsonProperty("titulo")
     @Column(nullable = false)
     private String nome;
 }

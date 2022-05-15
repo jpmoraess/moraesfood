36f2b437f7b8b5387fca8641b40517af778de142
jpmoraess <moraesdeveloperweb@gmail.com>
2022-05-15 18:35:41 -0300

    Implementando consultas JPQL em repositórios

--- src/main/java/br/com/moraesit/moraesfood/Controller.java
+++ src/main/java/br/com/moraesit/moraesfood/Controller.java
@@ -1,15 +1,29 @@
 package br.com.moraesit.moraesfood;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.moraesit.moraesfood.domain.entity.Cozinha;
+import br.com.moraesit.moraesfood.domain.repository.CozinhaRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/teste")
 public class Controller {
+
+    private final CozinhaRepository cozinhaRepository;
+
+    public Controller(CozinhaRepository cozinhaRepository) {
+        this.cozinhaRepository = cozinhaRepository;
+    }
 
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
         return "Hello!";
+    }
+
+    @GetMapping("/cozinhas")
+    public List<Cozinha> consultarPorNome(@RequestParam("nome") String nome) {
+        return cozinhaRepository.consultarPorNome(nome);
     }
 }
--- src/main/java/br/com/moraesit/moraesfood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/br/com/moraesit/moraesfood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -3,14 +3,14 @@ package br.com.moraesit.moraesfood.infrastructure.repository;
 import br.com.moraesit.moraesfood.domain.entity.Cozinha;
 import br.com.moraesit.moraesfood.domain.repository.CozinhaRepository;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@Component
+@Repository
 public class CozinhaRepositoryImpl implements CozinhaRepository {
 
     @PersistenceContext
@@ -19,6 +19,13 @@ public class CozinhaRepositoryImpl implements CozinhaRepository {
     @Override
     public List<Cozinha> listar() {
         return entityManager.createQuery("from Cozinha", Cozinha.class).getResultList();
+    }
+
+    @Override
+    public List<Cozinha> consultarPorNome(String nome) {
+        return entityManager.createQuery("from Cozinha where nome like :nome", Cozinha.class)
+                .setParameter("nome", "%" + nome + "%")
+                .getResultList();
     }
 
     @Override

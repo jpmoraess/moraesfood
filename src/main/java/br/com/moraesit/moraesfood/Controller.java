04b2946414f12a36d56c9f22a393f64a33cdb3ab
jpmoraess <moraesdeveloperweb@gmail.com>
2022-05-17 21:41:53 -0300

    Usando as keywords para definir critérios de query methods

--- src/main/java/br/com/moraesit/moraesfood/Controller.java
+++ src/main/java/br/com/moraesit/moraesfood/Controller.java
@@ -1,9 +1,12 @@
 package br.com.moraesit.moraesfood;
 
 import br.com.moraesit.moraesfood.domain.entity.Cozinha;
+import br.com.moraesit.moraesfood.domain.entity.Restaurante;
 import br.com.moraesit.moraesfood.domain.repository.CozinhaRepository;
+import br.com.moraesit.moraesfood.domain.repository.RestauranteRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,9 +15,11 @@ import java.util.Optional;
 public class Controller {
 
     private final CozinhaRepository cozinhaRepository;
+    private final RestauranteRepository restauranteRepository;
 
-    public Controller(CozinhaRepository cozinhaRepository) {
+    public Controller(CozinhaRepository cozinhaRepository, RestauranteRepository restauranteRepository) {
         this.cozinhaRepository = cozinhaRepository;
+        this.restauranteRepository = restauranteRepository;
     }
 
     @GetMapping("/hello")
@@ -25,11 +30,25 @@ public class Controller {
 
     @GetMapping("/cozinhas/por-nome")
     public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
-        return cozinhaRepository.findTodasByNome(nome);
+        return cozinhaRepository.findTodasByNomeContaining(nome);
     }
 
     @GetMapping("/cozinhas/unica-por-nome")
     public Optional<Cozinha> cozinhaPorNome(@RequestParam("nome") String nome) {
         return cozinhaRepository.findByNome(nome);
+    }
+
+    @GetMapping("/restaurantes/por-taxa-frete")
+    public List<Restaurante> restaurantesPorTaxaFrete(
+            @RequestParam("taxaInicial") BigDecimal taxaInicial,
+            @RequestParam("taxaFinal") BigDecimal taxaFinal) {
+        return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+    }
+
+    @GetMapping("/restaurantes/por-nome")
+    public List<Restaurante> restaurantesPorNome(
+            @RequestParam("nome") String nome,
+            @RequestParam("cozinhaId") Long cozinhaId) {
+        return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
     }
 }

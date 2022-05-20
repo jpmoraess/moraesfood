7742e4a8493794e57421c877263267cc85de5820
jpmoraess <moraesdeveloperweb@gmail.com>
2022-05-20 19:47:02 -0300

    Externalizando consultas JPQL para um arquivo XML

--- src/main/java/br/com/moraesit/moraesfood/domain/repository/RestauranteRepository.java
+++ src/main/java/br/com/moraesit/moraesfood/domain/repository/RestauranteRepository.java
@@ -15,8 +15,8 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 
     List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
-    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
-    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);  // usar @Param quando o nome do parâmetro for diferente.
+    //@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
 
     //List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
 

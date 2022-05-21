2c343c19a114a9e0e98036dd668331c225f84ba4
jpmoraess <moraesdeveloperweb@gmail.com>
2022-05-21 16:04:28 -0300

    Alterando a estratégia de fetching para Lazy Loading

--- src/main/java/br/com/moraesit/moraesfood/domain/entity/Restaurante.java
+++ src/main/java/br/com/moraesit/moraesfood/domain/entity/Restaurante.java
@@ -1,6 +1,7 @@
 package br.com.moraesit.moraesfood.domain.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -27,7 +28,9 @@ public class Restaurante {
     @Column(name = "tx_frete", nullable = false)
     private BigDecimal taxaFrete;
 
-    @ManyToOne
+    //@JsonIgnore
+    //@JsonIgnoreProperties("hibernateLazyInitializer")
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
 

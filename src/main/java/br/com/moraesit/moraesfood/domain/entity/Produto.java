1400b2c65f8d8042ecf44deafeaab1125750b40f
jpmoraess <moraesdeveloperweb@gmail.com>
2022-05-21 00:20:56 -0300

    Desafio: mapeando relacionamento muitos-para-um

---
+++ src/main/java/br/com/moraesit/moraesfood/domain/entity/Produto.java
@@ -0,0 +1,35 @@
+package br.com.moraesit.moraesfood.domain.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@EqualsAndHashCode(of = {"id"})
+@NoArgsConstructor
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private Boolean ativo = Boolean.TRUE;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurante_id", nullable = false)
+    private Restaurante restaurante;
+}

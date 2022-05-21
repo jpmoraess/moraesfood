e5854a9d57f1e783300cb2b54b067f779624b5ef
jpmoraess <moraesdeveloperweb@gmail.com>
2022-05-20 23:20:16 -0300

    Mapeando relacionamento muitos-para-muitos com @ManyToMany

--- src/main/java/br/com/moraesit/moraesfood/domain/entity/Restaurante.java
+++ src/main/java/br/com/moraesit/moraesfood/domain/entity/Restaurante.java
@@ -1,12 +1,12 @@
 package br.com.moraesit.moraesfood.domain.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.List;
 
 @Entity
 @Data
@@ -26,4 +26,10 @@ public class Restaurante {
     @ManyToOne
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
+
+    @ManyToMany
+    @JoinTable(name = "restaurante_forma_pagamento",
+            joinColumns = @JoinColumn(name = "restaurante_id"),
+            inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+    private List<FormaPagamento> formaPagamentos;
 }

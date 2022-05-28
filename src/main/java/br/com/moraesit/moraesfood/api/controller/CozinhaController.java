8fd25778b3a4b82bff7bbde2f6b111edb28f62c9
jpmoraess <moraesdeveloperweb@gmail.com>
2022-05-28 13:05:56 -0300

    Lançando exceções do tipo ResponseStatusException

--- src/main/java/br/com/moraesit/moraesfood/api/controller/CozinhaController.java
+++ src/main/java/br/com/moraesit/moraesfood/api/controller/CozinhaController.java
@@ -1,12 +1,15 @@
 package br.com.moraesit.moraesfood.api.controller;
 
 import br.com.moraesit.moraesfood.domain.entity.Cozinha;
+import br.com.moraesit.moraesfood.domain.exception.EntidadeEmUsoException;
+import br.com.moraesit.moraesfood.domain.exception.EntidadeNaoEncontradaException;
 import br.com.moraesit.moraesfood.domain.repository.CozinhaRepository;
 import br.com.moraesit.moraesfood.domain.service.CozinhaService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -68,6 +71,12 @@ public class CozinhaController {
     @DeleteMapping("/{cozinhaId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(@PathVariable Long cozinhaId) {
-        cozinhaService.remover(cozinhaId);
+        try {
+            cozinhaService.remover(cozinhaId);
+        } catch (EntidadeNaoEncontradaException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        } catch (EntidadeEmUsoException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        }
     }
 }
